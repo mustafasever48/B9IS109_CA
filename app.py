9e3d82cb398846af6264bef989f46ae93fcb4bf5
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-02-29 03:42:18 -0800

    logging

--- app.py
+++ app.py
@@ -4,5 +4,41 @@ from flask_cors import CORS
 import json
 from datetime import datetime
 from flask import jsonify
+mysql = mysql.connector.connect(
+    user='web',
+    password='webPass',
+    host='127.0.0.1',
+    database='rma'
+)
+import os
+print(os.getcwd())
 
+
+from logging.config import dictConfig
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+app.logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+
+
+app = Flask(__name__)
+CORS(app)
 
