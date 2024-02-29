82a409218a00b1c6158883f13f644da3b9c1e52c
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-02-29 03:47:04 -0800

    index page get/post test

--- app.py
+++ app.py
@@ -42,3 +42,72 @@ app.logger.addHandler(file_handler)
 app = Flask(__name__)
 CORS(app)
 
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(app, 'mysql') and app.mysql:
+        app.mysql.close()
+@app.route("/add", methods=['POST'])
+def add():
+    if request.method == 'POST':
+        brandName = request.form['Brand_Name']
+        modelName = request.form['Model_Name']
+        productName = request.form['Product_Name']
+        serialNumber = request.form['Serial_Number']
+        ProductSoldDate = request.form['Product_Sold_Date']
+
+        cur = mysql.cursor()
+
+        brand_s = 'INSERT INTO Brand(Brand_Name) VALUES(%s);'
+        cur.execute(brand_s, (brandName,))
+        mysql.commit()
+
+        model_s = 'INSERT INTO Model(Model_Name) VALUES(%s);'
+        cur.execute(model_s, (modelName,))
+        mysql.commit()
+
+        product_s = 'INSERT INTO Product(Product_Name, Serial_Number, Product_Sold_Date) VALUES(%s, %s, %s);'
+        cur.execute(product_s, (productName, serialNumber, ProductSoldDate))
+        mysql.commit()
+
+        cur.close()
+
+    else:
+        return render_template('add.html')
+
+    return '{"Result":"Success"}'
+
+@app.route("/", methods=['GET'])
+def hello():
+    serial_number = request.args.get('serial_number', '')
+
+    cur = mysql.cursor()
+
+    sql_query = '''
+        SELECT Brand.Brand_Name, Model.Model_Name, Product.Product_Name, Product.Serial_Number, Product.Product_Sold_Date
+        FROM Product
+        JOIN Model ON Product.Model_ID = Model.Model_ID
+        JOIN Brand ON Model.Brand_ID = Brand.Brand_ID
+        WHERE Product.Serial_Number = %s;
+    '''
+
+    cur.execute(sql_query, (serial_number,))
+    rv = cur.fetchall()
+
+    Results = []
+    for row in rv:
+        Result = {}
+        Result['Brand_Name'] = row[0].replace('\n', ' ')
+        Result['Model_Name'] = row[1]
+        Result['Product_Name'] = row[2]
+        Result['Serial_Number'] = row[3]
+        Result['Product_Sold_Date'] = row[4].isoformat() if row[4] else None
+        Results.append(Result)
+
+    response = {'Results': Results, 'count': len(Results)}
+    ret = app.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return ret
