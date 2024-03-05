b896e8656366196caa72a0909ca0c022ff97ee75
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-03-05 20:03:13 +0000

    app.logger.setLevel(logging.DEBUG)

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ CORS(app)
 print(os.getcwd())
 
 # Logger with file on the same directory
-app.logger.setLevel(logging.DEBUG)
+app.logger.setLevel(logging.DEBUG)  
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler = logging.FileHandler('app.log')
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.DEBUG) 
 file_handler.setFormatter(formatter)
 app.logger.addHandler(file_handler)
 
