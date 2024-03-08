93e47c8165466edd11c1a77da0fa27f0221a0ec2
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-03-08 17:28:51 +0000

    return redirect(url_for('/technical'))

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def login():
         if user:
             session['user_id'] = user['Technician_ID']
             flash('Login successful!', 'success')
-            return redirect(url_for('technical_page'))
+            return redirect(url_for('/technical'))
         else:
             flash('Invalid credentials. Please try again.', 'danger')
 
