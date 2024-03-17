e975492db8c48418c80f3a640d042edff6674e1d
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-03-17 21:53:55 +0000

    migrate = Migrate(app, db)

--- app.py
+++ app.py
@@ -9,6 +9,20 @@ from logging.config import dictConfig
 import os
 import traceback  
 
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db  
+
+
+migrate = Migrate(app, db)
+
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+
 mysql = mysql.connector.connect(
     user='web',
     password='webPass',
