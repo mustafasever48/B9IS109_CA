879b4b26f9c73e27b9e93022d20460adfdaad94b
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-03-17 21:12:17 +0000

    return render_template('index.html')

--- app.py
+++ app.py
@@ -381,7 +381,7 @@ def login():
             return redirect(url_for('/technical'))
         else:
             return 'Invalid email or password. Please try again.'
-    return render_template('index')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
