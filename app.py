bbb33ff0bb0b566fa4feec642dc6511ba07abf27
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-03-19 16:58:31 +0000

    return redirect(url_for('/technical'))

--- app.py
+++ app.py
@@ -387,7 +387,7 @@ def login():
                 print(technician)
                 # session['loggedin'] = True
                 # session['email'] = technician[4]
-                return redirect(url_for('technical/index.html'))
+                return redirect(url_for('/technical'))
             else:
                 abort(400, 'Invalid email or password. Please try again.')
         except Exception as e:
