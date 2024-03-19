87c6174f342b8b76713681e409a2a05cda29f924
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-03-19 15:35:51 +0000

    return redirect(url_for('/'))

--- app.py
+++ app.py
@@ -387,7 +387,7 @@ def login():
                 print(technician)
                 # session['loggedin'] = True
                 # session['email'] = technician[4]
-                return redirect(url_for('https://msubuntu.northeurope.cloudapp.azure.com/technical/'))
+                return redirect(url_for('/'))
             else:
                 abort(400, 'Invalid email or password. Please try again.')
         except Exception as e:
