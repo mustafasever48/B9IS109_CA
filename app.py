5cc3fd6d17794f6e9b32599f3ac93613be82a94c
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-03-19 21:51:28 +0000

    return send_from_directory('/var/www/html/login', 'index.html')

--- app.py
+++ app.py
@@ -392,6 +392,7 @@ def login():
             else:
                 error_message = 'Invalid email or password. Please try again.'
                 abort(400, error_message)
+                return send_from_directory('/var/www/html/login', 'index.html')
         except Exception as e:
             abort(500, str(e))
         finally:
