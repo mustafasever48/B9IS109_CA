99d650924f5c85ef36e6efcf5833009be0f2d388
Mustafa SEVER <82729984+mustafasever48@users.noreply.github.com>
2024-03-24 02:47:49 +0000

    @app.route('/technical', methods=['GET']) def technical_page():     cur = mysql.cursor(dictionary=True)
        rma_status_query = '''
            SELECT RMA.RMA_ID, RMA.Inspaction_Start_Date, RMA.Inspeciton_Completion_Date, RMA.Product_Defect,
                   RMA.Check_Issue, RMA.Result_Issue, RMA.Product_ID, Product.Serial_Number, Product.Product_Name,
                   Technician.Technician_ID
            FROM RMA
            LEFT JOIN Product ON RMA.Product_ID = Product.Product_ID
            LEFT JOIN Technician ON RMA.Technician_ID = Technician.Technician_ID
        '''
        
        cur.execute(rma_status_query)
        rma_status = cur.fetchall()
    
        cur.close()
    
        return jsonify(rma_status)

--- app.py
+++ app.py
@@ -194,11 +194,7 @@ def check_rma_status():
 
 @app.route('/technical', methods=['GET'])
 def technical_page():
-    if 'loggedin' not in session:
-        print('loggedin' not in session)
-        return send_from_directory('/var/www/html/login', 'index.html')
-    else:
-        cur = mysql.cursor(dictionary=True)
+    cur = mysql.cursor(dictionary=True)
 
     rma_status_query = '''
         SELECT RMA.RMA_ID, RMA.Inspaction_Start_Date, RMA.Inspeciton_Completion_Date, RMA.Product_Defect,
